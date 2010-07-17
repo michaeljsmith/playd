9cf8c0d30e155cbd0be6869e54b93d6096e81b61
Michael Smith <msmith@msmith.id.au>
2010-07-17 13:22:46 +1000

    Thread structure progress.

--- playlist.py
+++ playlist.py
@@ -1,7 +1,64 @@
 #!/usr/bin/env python3.1
 
+from threading import Thread, Lock, Semaphore
+from time import sleep
+
+class ThreadDataQueue(object):
+	def __init__(self):
+		self.items = []
+		self.lock = Lock()
+		self.semaphore = Semaphore(0)
+
+	def append(self, message):
+		self.lock.acquire()
+		try:
+			self.items.append(message)
+			self.semaphore.release()
+		finally:
+			self.lock.release()
+
+	def pop(self):
+		self.semaphore.acquire()
+		self.lock.acquire()
+		message = None
+		try:
+			message = self.items.pop(0)
+		finally:
+			self.lock.release()
+		return message
+
+class PlayThread(Thread):
+
+	def __init__(self, queue):
+		super().__init__()
+		self.queue = queue
+
+	def run(self):
+		while True:
+			x = self.queue.pop()
+			print(x)
+
+class ReadThread(Thread):
+
+	def __init__(self, queue):
+		super().__init__()
+		self.queue = queue
+
+	def run(self):
+		a = 0
+		while True:
+			sleep(1.0)
+			self.queue.append(str(a))
+			a += 1
+
 def main():
-	print('hello')
+	items = ThreadDataQueue()
+	play_thread = PlayThread(items)
+	play_thread.start()
+	read_thread = ReadThread(items)
+	read_thread.start()
+	play_thread.join()
+	read_thread.join()
 
 if __name__ == '__main__':
 	main()
