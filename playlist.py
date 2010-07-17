55c1c3d019dd53c5296563ffc6e8734f709a104c
Michael Smith <msmith@msmith.id.au>
2010-07-17 18:12:11 +1000

    Playlist progress.

--- playlist.py
+++ playlist.py
@@ -2,6 +2,7 @@
 
 from threading import Thread, Lock, Semaphore
 from time import sleep
+from sys import stdin
 
 class ThreadDataQueue(object):
 	def __init__(self):
@@ -27,38 +28,101 @@ class ThreadDataQueue(object):
 			self.lock.release()
 		return message
 
+	def __len__(self):
+		self.lock.acquire()
+		l = -1
+		try:
+			l = len(self.items)
+		finally:
+			self.lock.release()
+		return l
+
 class PlayThread(Thread):
 
-	def __init__(self, queue):
+	class QueueListenThread(Thread):
+		def __init__(self, in_queue, out_queue, semaphore):
+			super().__init__()
+			self.in_queue = in_queue
+			self.out_queue = out_queue
+			self.semaphore = semaphore
+
+		def run(self):
+			while True:
+				x = self.in_queue.pop()
+				if not x: break
+				self.out_queue.append(x)
+				self.semaphore.release()
+
+	class ActionThread(Thread):
+		def __init__(self, action, arg, oob_queue, semaphore):
+			super().__init__()
+			self.action = action
+			self.arg = arg
+			self.oob_queue = oob_queue
+			self.semaphore = semaphore
+
+		def run(self):
+			self.action(self.arg)
+			self.oob_queue.append(('finished'))
+			self.semaphore.release()
+
+	def __init__(self, action, queue):
 		super().__init__()
+		self.action = action
+		self.semaphore = Semaphore(0)
+		self.file_queue = ThreadDataQueue()
+		self.oob_queue = ThreadDataQueue()
 		self.queue = queue
+		self.queue_listen_thread = PlayThread.QueueListenThread(queue, self.file_queue, self.semaphore)
+		self.queue_listen_thread.start()
+		self.action_thread = None
 
 	def run(self):
 		while True:
-			x = self.queue.pop()
-			print(x)
+			self.semaphore.acquire()
+			if len(self.oob_queue):
+				cmd = self.oob_queue.pop()
+				if cmd[0] == 'finished':
+					self.action_thread.join()
+					self.action_thread = None
+				elif cmd[0] == 'exit':
+					self.queue.append(None)
+					self.queue_listen_thread.join()
+					break
+			elif not self.action_thread:
+				x = self.file_queue.pop()
+				self.action_thread = PlayThread.ActionThread(
+						self.action, x, self.oob_queue, self.semaphore)
+				self.action_thread.start()
+
+	def exit(self):
+		self.oob_queue.append(('exit'))
+		self.semaphore.release()
 
 class ReadThread(Thread):
 
-	def __init__(self, queue):
+	def __init__(self, queue, input):
 		super().__init__()
 		self.queue = queue
+		self.input = input
 
 	def run(self):
-		a = 0
-		while True:
-			sleep(1.0)
-			self.queue.append(str(a))
-			a += 1
+		for line in self.input:
+			self.queue.append(line)
+
+def action(x):
+	sleep(1.0)
+	print(x)
 
 def main():
 	items = ThreadDataQueue()
-	play_thread = PlayThread(items)
+	play_thread = PlayThread(action, items)
 	play_thread.start()
-	read_thread = ReadThread(items)
+	read_thread = ReadThread(items, stdin)
 	read_thread.start()
-	play_thread.join()
 	read_thread.join()
+	play_thread.exit()
+	play_thread.join()
 
 if __name__ == '__main__':
 	main()
