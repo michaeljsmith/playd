a67cd5bf03c57b892205bb01f2e7339b779a70e7
Michael Smith <msmith@msmith.id.au>
2010-07-17 18:37:02 +1000

    Basic thread structure working.

--- playlist.py
+++ playlist.py
@@ -4,6 +4,10 @@ from threading import Thread, Lock, Semaphore
 from time import sleep
 from sys import stdin
 
+def trace(x):
+	#print(x)
+	pass
+
 class ThreadDataQueue(object):
 	def __init__(self):
 		self.items = []
@@ -47,11 +51,16 @@ class PlayThread(Thread):
 			self.semaphore = semaphore
 
 		def run(self):
+			trace('Queue listen thread started.')
+
 			while True:
 				x = self.in_queue.pop()
 				if not x: break
+				trace('Queue listen thread found item (' + x + ')')
 				self.out_queue.append(x)
 				self.semaphore.release()
+
+			trace('Queue listen thread ended.')
 
 	class ActionThread(Thread):
 		def __init__(self, action, arg, oob_queue, semaphore):
@@ -62,8 +71,10 @@ class PlayThread(Thread):
 			self.semaphore = semaphore
 
 		def run(self):
+			trace('Action (' + self.arg + ') started.')
 			self.action(self.arg)
-			self.oob_queue.append(('finished'))
+			trace('Action (' + self.arg + ') ended.')
+			self.oob_queue.append(('finished',))
 			self.semaphore.release()
 
 	def __init__(self, action, queue):
@@ -78,25 +89,34 @@ class PlayThread(Thread):
 		self.action_thread = None
 
 	def run(self):
+		trace('Play thread started.')
+
 		while True:
 			self.semaphore.acquire()
 			if len(self.oob_queue):
 				cmd = self.oob_queue.pop()
 				if cmd[0] == 'finished':
+					trace('Play thread got command (' + cmd[0] + ')')
 					self.action_thread.join()
 					self.action_thread = None
 				elif cmd[0] == 'exit':
+					trace('Play thread got command (' + cmd[0] + ')')
 					self.queue.append(None)
 					self.queue_listen_thread.join()
 					break
-			elif not self.action_thread:
+				else:
+					trace('Play thread got UNKNOWN command (' + cmd[0] + ')')
+			if not self.action_thread and len(self.file_queue):
 				x = self.file_queue.pop()
+				trace('Play thread performing new item (' + x + ')')
 				self.action_thread = PlayThread.ActionThread(
 						self.action, x, self.oob_queue, self.semaphore)
 				self.action_thread.start()
 
+		trace('Play thread ended.')
+
 	def exit(self):
-		self.oob_queue.append(('exit'))
+		self.oob_queue.append(('exit',))
 		self.semaphore.release()
 
 class ReadThread(Thread):
@@ -107,22 +127,31 @@ class ReadThread(Thread):
 		self.input = input
 
 	def run(self):
+		trace('Read thread started.')
 		for line in self.input:
-			self.queue.append(line)
+			self.queue.append(line.strip())
+		trace('Read thread ended.')
 
 def action(x):
 	sleep(1.0)
 	print(x)
 
 def main():
+	trace('Main thread started.')
 	items = ThreadDataQueue()
 	play_thread = PlayThread(action, items)
+	trace('Main thread launching play thread.')
 	play_thread.start()
 	read_thread = ReadThread(items, stdin)
+	trace('Main thread launching read thread.')
 	read_thread.start()
+	trace('Main thread joining read thread.')
 	read_thread.join()
+	trace('Main thread exiting play thread.')
 	play_thread.exit()
+	trace('Main thread joining play thread.')
 	play_thread.join()
+	trace('Main thread ended')
 
 if __name__ == '__main__':
 	main()
