08ae8b2f377dff2880e07f43f7daf88849102329
Michael Smith <msmith@msmith.id.au>
2010-07-18 14:27:08 +1000

    Rejigged thread framework.

--- playlist.py
+++ playlist.py
@@ -1,158 +1,140 @@
 #!/usr/bin/env python3.1
 
-from threading import Thread, Lock, Semaphore
+from threading import Thread, Lock, Condition
 from time import sleep
 from sys import stdin
 
 def trace(x):
-	#print(x)
-	pass
-
-class ThreadDataQueue(object):
-	def __init__(self):
-		self.items = []
-		self.lock = Lock()
-		self.semaphore = Semaphore(0)
-
-	def append(self, message):
-		self.lock.acquire()
-		try:
-			self.items.append(message)
-			self.semaphore.release()
-		finally:
-			self.lock.release()
-
-	def pop(self):
-		self.semaphore.acquire()
-		self.lock.acquire()
-		message = None
-		try:
-			message = self.items.pop(0)
-		finally:
-			self.lock.release()
-		return message
-
-	def __len__(self):
-		self.lock.acquire()
-		l = -1
-		try:
-			l = len(self.items)
-		finally:
-			self.lock.release()
-		return l
+	print(x)
 
 class PlayThread(Thread):
 
-	class QueueListenThread(Thread):
-		def __init__(self, in_queue, out_queue, semaphore):
-			super().__init__()
-			self.in_queue = in_queue
-			self.out_queue = out_queue
-			self.semaphore = semaphore
-
-		def run(self):
-			trace('Queue listen thread started.')
-
-			while True:
-				x = self.in_queue.pop()
-				if not x: break
-				trace('Queue listen thread found item (' + x + ')')
-				self.out_queue.append(x)
-				self.semaphore.release()
-
-			trace('Queue listen thread ended.')
-
-	class ActionThread(Thread):
-		def __init__(self, action, arg, oob_queue, semaphore):
-			super().__init__()
-			self.action = action
-			self.arg = arg
-			self.oob_queue = oob_queue
-			self.semaphore = semaphore
-
-		def run(self):
-			trace('Action (' + self.arg + ') started.')
-			self.action(self.arg)
-			trace('Action (' + self.arg + ') ended.')
-			self.oob_queue.append(('finished',))
-			self.semaphore.release()
-
-	def __init__(self, action, queue):
+	def __init__(self, action_performer):
 		super().__init__()
-		self.action = action
-		self.semaphore = Semaphore(0)
-		self.file_queue = ThreadDataQueue()
-		self.oob_queue = ThreadDataQueue()
-		self.queue = queue
-		self.queue_listen_thread = PlayThread.QueueListenThread(queue, self.file_queue, self.semaphore)
-		self.queue_listen_thread.start()
-		self.action_thread = None
+		self.action_performer = action_performer
+		self._queue = []
+		self.exit_pending = False
+		self.action = None
+		self.condition = Condition()
 
 	def run(self):
 		trace('Play thread started.')
 
-		while True:
-			self.semaphore.acquire()
-			if len(self.oob_queue):
-				cmd = self.oob_queue.pop()
-				if cmd[0] == 'finished':
-					trace('Play thread got command (' + cmd[0] + ')')
-					self.action_thread.join()
-					self.action_thread = None
-				elif cmd[0] == 'exit':
-					trace('Play thread got command (' + cmd[0] + ')')
-					self.queue.append(None)
-					self.queue_listen_thread.join()
+		action_running = [False]
+
+		self.condition.acquire()
+		try:
+			while True:
+				def on_action_finished():
+					self.condition.acquire()
+					try:
+						action_running[0] = False
+						self.condition.notify_all()
+					finally:
+						self.condition.release()
+
+				if self.exit_pending:
+					if action_running[0]:
+						trace('Play thread cancelling action.')
+						self.action.cancel()
 					break
-				else:
-					trace('Play thread got UNKNOWN command (' + cmd[0] + ')')
-			if not self.action_thread and len(self.file_queue):
-				x = self.file_queue.pop()
-				trace('Play thread performing new item (' + x + ')')
-				self.action_thread = PlayThread.ActionThread(
-						self.action, x, self.oob_queue, self.semaphore)
-				self.action_thread.start()
+
+				if not action_running[0]:
+					if self._queue:
+						x = self._queue.pop(0)
+						trace('Play threading performing action (' + x + ')')
+						self.action = self.action_performer(x,
+								on_action_finished, on_action_finished)
+						action_running[0] = True
+					else:
+						trace('Play thread waiting for input.')
+						self.action = None
+
+				trace('Play thread waiting.')
+				self.condition.wait()
+				trace('Play thread woken.')
+		finally:
+			self.condition.release()
 
 		trace('Play thread ended.')
 
 	def exit(self):
-		self.oob_queue.append(('exit',))
-		self.semaphore.release()
+		trace('Cancelling play thread.')
+		self.condition.acquire()
+		try:
+			self._queue[:] = [None]
+			self.exit_pending = True
+			self.condition.notify_all()
+		finally:
+			self.condition.release()
 
-class ReadThread(Thread):
+		trace('Waiting for play thread.')
+		self.join()
+		trace('Finished waiting for play thread.')
 
-	def __init__(self, queue, input):
-		super().__init__()
-		self.queue = queue
-		self.input = input
+	def queue(self, x):
+		trace('Queueing (' + x + ') on play thread.')
+		self.condition.acquire()
+		try:
+			self._queue.append(x)
+			self.condition.notify_all()
+		finally:
+			self.condition.release()
 
-	def run(self):
-		trace('Read thread started.')
-		for line in self.input:
-			self.queue.append(line.strip())
-		trace('Read thread ended.')
+class ActionPerformer(object):
 
-def action(x):
-	sleep(1.0)
-	print(x)
+	class ActionThread(Thread):
+
+		def __init__(self, x, condition, on_finished, on_cancelled):
+			super().__init__()
+			self.x = x
+			self.condition = condition
+			self.cancelled = False
+			self.on_finished = on_finished
+			self.on_cancelled = on_cancelled
+
+		def run(self):
+			self.condition.acquire()
+			try:
+				self.condition.wait(2.0)
+				if not self.cancelled:
+					print('out: ' + self.x)
+			finally:
+				self.condition.release()
+			if not self.cancelled:
+				self.on_finished()
+
+		def cancel(self):
+			self.condition.acquire()
+			try:
+				self.cancelled = True
+				self.condition.notify_all()
+			finally:
+				self.condition.release()
+
+	def __init__(self, x, on_finished, on_cancelled):
+		self.condition = Condition()
+		self.thread = self.ActionThread(x, self.condition, on_finished, on_cancelled)
+		self.thread.start()
+
+	def cancel(self):
+		self.thread.cancel()
+		self.thread.join()
 
 def main():
 	trace('Main thread started.')
-	items = ThreadDataQueue()
-	play_thread = PlayThread(action, items)
+	play_thread = PlayThread(ActionPerformer)
 	trace('Main thread launching play thread.')
 	play_thread.start()
-	read_thread = ReadThread(items, stdin)
-	trace('Main thread launching read thread.')
-	read_thread.start()
-	trace('Main thread joining read thread.')
-	read_thread.join()
+
+	for line in stdin:
+		if line[-1] == '\n':
+			line = line[:-1]
+		play_thread.queue(line)
+
 	trace('Main thread exiting play thread.')
 	play_thread.exit()
-	trace('Main thread joining play thread.')
-	play_thread.join()
 	trace('Main thread ended')
 
 if __name__ == '__main__':
 	main()
-
